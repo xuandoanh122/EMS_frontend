e83f1a543034429f6f695311625c70330b944dee
Doanh Nguyen <doanh.nguyen@eiu.edu.vn>
2026-03-13 19:27:40 +0700

    fix blank page

--- src/components/animations/AnimatedRoutes.tsx
+++ src/components/animations/AnimatedRoutes.tsx
@@ -1,21 +1,13 @@
+import { useEffect } from 'react'
 import { useLocation, Outlet } from 'react-router-dom'
-import { motion, AnimatePresence } from 'framer-motion'
 
 export function AnimatedRoutes() {
     const location = useLocation()
 
-    return (
-        <AnimatePresence mode="sync">
-            <motion.div
-                key={location.pathname}
-                initial={{ opacity: 0 }}
-                animate={{ opacity: 1 }}
-                exit={{ opacity: 0 }}
-                transition={{ duration: 0.2 }}
-                className="h-full w-full"
-            >
-                <Outlet />
-            </motion.div>
-        </AnimatePresence>
-    )
+    // Reset scroll to top when location changes
+    useEffect(() => {
+        window.scrollTo({ top: 0, behavior: 'auto' })
+    }, [location.pathname])
+
+    return <Outlet />
 }
